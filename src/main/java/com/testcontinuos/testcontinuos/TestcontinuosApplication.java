cc6fb5675c24dfc87edcf5077f4254940aca0c92
jimmy Muchachasoy <jmuchachasoy@personalsoft.com.co>
2019-10-05 19:51:02 +0000

    first commit

---
+++ src/main/java/com/testcontinuos/testcontinuos/TestcontinuosApplication.java
@@ -0,0 +1,13 @@
+package com.testcontinuos.testcontinuos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestcontinuosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestcontinuosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/testcontinuos/testcontinuos/TestcontinuosApplicationTests.java
@@ -0,0 +1,16 @@
+package com.testcontinuos.testcontinuos;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TestcontinuosApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
